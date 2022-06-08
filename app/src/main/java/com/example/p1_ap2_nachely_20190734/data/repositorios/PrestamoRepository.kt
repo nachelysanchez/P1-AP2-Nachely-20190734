4a4924859ff7739aa10184bbd6c27b5a54283267
nachelysanchez <nashely-5@hotmail.com>
2022-06-08 18:38:59 -0400

    Creando el repositorio del Prestamo

--- app/src/main/java/com/example/p1_ap2_nachely_20190734/data/repositorios/PrestamoRepository.kt
+++ app/src/main/java/com/example/p1_ap2_nachely_20190734/data/repositorios/PrestamoRepository.kt
@@ -1,4 +1,25 @@
 package com.example.p1_ap2_nachely_20190734.data.repositorios
 
-class PrestamoRepository {
+import com.example.p1_ap2_nachely_20190734.data.PrestamosDao
+import com.example.p1_ap2_nachely_20190734.model.Prestamos
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class PrestamoRepository @Inject constructor(
+    val prestamosDao: PrestamosDao
+){
+    suspend fun insertar(prestamos: Prestamos){
+        prestamosDao.insertar(prestamos = prestamos)
+    }
+    suspend fun eliminar(prestamos: Prestamos){
+        prestamosDao.eliminar(prestamos = prestamos)
+    }
+
+     fun buscar(prestamoId : Int) : Flow<Prestamos> {
+        return prestamosDao.buscar(prestamoId = prestamoId)
+    }
+
+    fun getList() : Flow<List<Prestamos>>{
+        return prestamosDao.getList()
+    }
 }
