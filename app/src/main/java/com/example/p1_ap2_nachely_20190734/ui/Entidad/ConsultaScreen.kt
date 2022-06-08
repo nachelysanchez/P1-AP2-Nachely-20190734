2933aeaa53860223e1463281448f6eb3f1c544ea
nachelysanchez <nashely-5@hotmail.com>
2022-06-08 01:44:41 -0400

    Creando ConsultaScreen.kt

---
+++ app/src/main/java/com/example/p1_ap2_nachely_20190734/ui/Entidad/ConsultaScreen.kt
@@ -0,0 +1,30 @@
+package com.example.p1_ap2_nachely_20190734.ui.Entidad
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.example.p1_ap2_nachely_20190734.utils.Screen
+
+@Composable
+fun ConsultaScreen(
+    navHostController: NavHostController
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = Screen.RegistroScreen.route) }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                navHostController.navigate("")
+            }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+    ) {
+
+    }
+}
--- app/src/main/java/com/example/p1_ap2_nachely_20190734/utils/Screen.kt
+++ app/src/main/java/com/example/p1_ap2_nachely_20190734/utils/Screen.kt
@@ -1,4 +1,5 @@
 package com.example.p1_ap2_nachely_20190734.utils
 
 sealed class Screen (val route :String){
+    object RegistroScreen : Screen("Registro")
 }
