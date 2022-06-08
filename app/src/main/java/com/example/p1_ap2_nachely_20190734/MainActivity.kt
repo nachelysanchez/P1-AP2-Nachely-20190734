c0a064b857f203eef6b601bd2fe7b58315426f9a
nachelysanchez <nashely-5@hotmail.com>
2022-06-08 01:35:03 -0400

    Agregando AndroidPointEntry en el MainActivity, y creando la clase Screen.kt

--- app/src/main/java/com/example/p1_ap2_nachely_20190734/MainActivity.kt
+++ app/src/main/java/com/example/p1_ap2_nachely_20190734/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.p1_ap2_nachely_20190734.ui.theme.P1AP2Nachely20190734Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/p1_ap2_nachely_20190734/utils/Screen.kt
@@ -0,0 +1,4 @@
+package com.example.p1_ap2_nachely_20190734.utils
+
+sealed class Screen (val route :String){
+}
